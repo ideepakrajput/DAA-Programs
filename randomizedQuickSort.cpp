62e5d56029ad661dd8a242c0fb375a183dc17f98
ideepakrajput <deepakkumar18112001@gmail.com>
2022-02-15 23:40:15 +0530

    heapSort

--- randomizedQuickSort.cpp
+++ randomizedQuickSort.cpp
@@ -86,15 +86,6 @@ void printArray(int arr[], int size)
 // Driver Code
 int main()
 {
-    // int arr[] = {10, 7, 8, 9, 1, 5};
-    // int n = sizeof(arr) / sizeof(arr[0]);
-
-    // quickSort(arr, 0, n - 1);
-    // printf("Sorted array: \n");
-    // printArray(arr, n);
-
-    // return 0;
-
     int n;
     cout << "Enter the number of elements in array :- ";
     cin >> n;
